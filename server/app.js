83ae0ea83728bb37a883c3380ae91e3085ae4e02
frankdb <fd.barros@yahoo.com>
2019-03-12 19:12:30 -0700

    Init commit

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
+const axios = require("axios");
 
+const API_KEY = "63eedeac";
 
 const app = express();
 
+app.use(morgan("dev"));
+
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+
+app.get("/", (req, res) => {
+  // check url parameters using req.query
+  axios
+    .get("http://omdbapi.com/?i=" + "tt3896198" + `&apikey=${API_KEY}`)
+    .then(response => console.log(response));
+});
 
 module.exports = app;
