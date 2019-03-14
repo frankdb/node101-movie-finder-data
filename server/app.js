8512c8cb61c7b3ea2f56e283349c3b501e9c4f4b
frankdb <fd.barros@yahoo.com>
2019-03-13 17:49:57 -0700

    Got cache working

--- server/app.js
+++ server/app.js
@@ -10,21 +10,37 @@ app.use(morgan("dev"));
 
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
 
+let cache = {};
+
 app.get("/", (req, res) => {
   // check url parameters using req.query
   // let search = req.query;
   // console.log(search);
 
   if (req.query.i) {
-    axios
-      .get("https://omdbapi.com/?i=" + `${req.query.i}` + `&apikey=${API_KEY}`)
-      .then(response => console.log(response))
-      .catch(err => console.log(err));
+    if (cache[req.query.i]) {
+      res.send(cache[req.query.i]);
+    } else {
+      axios
+        .get("https://omdbapi.com/?i=" + `${req.query.i}` + `&apikey=8730e0e`)
+        .then(response => {
+          cache[req.query.i] = response.data;
+          res.send(cache[req.query.i]);
+        })
+        .catch(err => console.log(err));
+    }
   } else if (req.query.t) {
-    axios
-      .get("https://omdbapi.com/?t=" + `${req.query.t}` + `&apikey=${API_KEY}`)
-      .then(response => console.log(response))
-      .catch(err => console.log(err));
+    if (cache[req.query.t]) {
+      res.send(cache[req.query.t]);
+    } else {
+      axios
+        .get("https://omdbapi.com/?t=" + `${req.query.t}` + `&apikey=8730e0e`)
+        .then(response => {
+          cache[req.query.t] = response.data;
+          res.send(cache[req.query.t]);
+        })
+        .catch(err => console.log(err));
+    }
   }
 });
 
